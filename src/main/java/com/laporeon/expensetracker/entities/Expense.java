816adc795993ccb13c83a6b3cb3d2fb2912143a5
laporeon <175569928+laporeon@users.noreply.github.com>
2026-06-03 20:06:20 -0300

    refactor(api): remove create entity logic from entities

--- src/main/java/com/laporeon/expensetracker/entities/Expense.java
+++ src/main/java/com/laporeon/expensetracker/entities/Expense.java
@@ -1,6 +1,5 @@
 package com.laporeon.expensetracker.entities;
 
-import com.laporeon.expensetracker.dtos.request.UpdateExpenseRequestDTO;
 import com.laporeon.expensetracker.enums.Category;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,7 +12,7 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.dialect.type.PostgreSQLEnumJdbcType;
@@ -25,10 +24,10 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "expenses")
-@Getter
+@Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder(access = AccessLevel.PRIVATE)
+@Builder
 public class Expense {
 
     @Id
@@ -60,27 +59,5 @@ public class Expense {
 
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
-
-    public static Expense create(String name, String description, BigDecimal amount, Category category, UUID userId, LocalDate date) {
-        return Expense.builder()
-                      .name(name)
-                      .description(description)
-                      .amount(amount)
-                      .category(category)
-                      .userId(userId)
-                      .date(date)
-                      .createdAt(Instant.now())
-                      .updatedAt(Instant.now())
-                      .build();
-    }
-
-    public void update(UpdateExpenseRequestDTO dto) {
-        if (dto.name() != null)        this.name = dto.name();
-        if (dto.description() != null) this.description = dto.description();
-        if (dto.amount() != null)      this.amount = dto.amount();
-        if (dto.category() != null)    this.category = Category.fromString(dto.category());
-        if (dto.date() != null)        this.date = dto.date();
-        this.updatedAt = Instant.now();
-    }
 
 }
